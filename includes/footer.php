6e90d6518488763905fe688064b341dee795b072
Leonardo Alvarez <leo92alvarez25@gmail.com>
2023-11-13 23:37:56 -0300

    firma & eliminado de contacto

--- includes/footer.php
+++ includes/footer.php
@@ -30,8 +30,7 @@
         <li><a class="link-footer" href="index.php" target="_blank" rel="noopener noreferrer">Inicio</a></li>
         <li><a class="link-footer" href="#" target="_blank" rel="noopener noreferrer">Hace tu Pedido</a></li>
         <li><a class="link-footer" href="#" target="_blank" rel="noopener noreferrer">Nuestros Productos</a></li>
-        <li><a class="link-footer" href="#" target="_blank" rel="noopener noreferrer">Contacto</a></li>
-        <li><a class="link-footer" href="login.php" target="_blank" rel="noopener noreferrer">Login para Empleados</a></li>
+        <li><a class="link-footer" href="loginForm.php" target="_blank" rel="noopener noreferrer">Login para Empleados</a></li>
       </ul>
     </div>
     <div class="col-lg-4 offset-lg-0 col-sm-8 offset-sm-2">
@@ -64,8 +63,15 @@
       <b>La Fuente</b>©. Proveedores de agua embotellada.
       <span id="dateYear"></span>
     </h6>
+    <p class="text-devs">Creado por: Alvarez, Benitez, Mendoza y Ojeda.</p>
   </div>
 </footer>
+<script>
+      const fecha = new Date();
+      const año = fecha.getFullYear();
+      const year= document.getElementById("dateYear");
+      year.innerHTML= año;
+    </script>
 <script src="js/jquery-3.6.0.min.js"></script>
 <script src="js/login.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
