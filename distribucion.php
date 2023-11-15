33fb967313ad9869a0bfe1b939cd84103aa87694
[Ding.dani[1;7r[1;7r[1;7r[1;7s[1;7s[1;7s <ing.dani@live.com.ar>
2023-11-15 17:56:21 +0100

    distribucion

--- distribucion.php
+++ distribucion.php
@@ -2,7 +2,9 @@
 include("includes/header.php");
 require("conn.php");
 
-$sql ="SELECT * FROM 'pedidos'"
+$sql ="SELECT p.id_pedido AS idPedido, c.nombre AS Nombre_cliente, c.telefono AS telefono, d.calle AS Direccion, d.numeracion AS altura, b.nombre_barrio AS barrio, b.zona AS distrito, m.tipo_pago AS metodo_de_pago, w.nombre_estado AS estado_de_pago, p.fecha_entrega AS dia_entrega FROM pedidos p INNER JOIN clientes c ON p.cliente_id = c.id_cliente INNER JOIN direcciones d ON c.dirreccion_id = d.id_direccion INNER JOIN barrios b ON b.id_barrio = d.barrio_id INNER JOIN metodos_Pago m ON p.metPago_id = m.id_metodo_pago INNER JOIN pagos_stados w ON p.statusPago_id = w.id_estado WHERE p.fecha_entrega IS NULL ORDER BY `distrito` ASC";
+
+$resultado=mysqli_query($connection ,$sql);
 ?>
 <section>
     <h1 class="text-center">Lista de distribucion
@@ -24,17 +26,32 @@ $sql ="SELECT * FROM 'pedidos'"
     </tr>
   </thead>
   <tbody>
-
-    <tr class="table-active">
-      <th scope="row">Active</th>
-      <td>hola</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
+<?php
+if(mysqli_num_rows($resultado)>0){
+  while($pedido=mysqli_fetch_array($resultado)){
+    echo"<tr>";
+      
+      echo"<td>". $pedido["idPedido"] ."</td>";
+      echo"<td>". $pedido["Nombre_cliente"] ."</td>";
+      echo"<td>". $pedido["telefono"] ."</td>";
+      echo"<td>". $pedido["Direccion"] ."</td>";
+      echo"<td>". $pedido["altura"] ."</td>";
+      echo"<td>". $pedido["barrio"] ."</td>";
+      echo"<td>". $pedido["distrito"] ."</td>";
+      echo"<td>". $pedido["metodo_de_pago"] ."</td>";
+      echo"<td>". $pedido["estado_de_pago"] ."</td>";
+      echo"<td>". $pedido["dia_entrega"] ."</td>";
+      
+      
+      
+      
+      
+      
+    echo"</tr>";
+  }
+}
+?>
+   
   </tbody>
 </table>
 </section>
