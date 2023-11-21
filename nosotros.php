78ee506cbe0109f41743a5bf7823d63eb71bc3a4
Leonardo Alvarez <leo92alvarez25@gmail.com>
2023-11-21 11:47:13 -0300

    leo cambios

--- nosotros.php
+++ nosotros.php
@@ -3,7 +3,12 @@ include("includes/header.php");
 ?>
 <section class="nosotros">
   <h1 class="text-center">conoce La Fuente</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, beatae non mollitia dicta recusandae eveniet dolor vitae adipisci deserunt architecto, natus, aliquid nam sequi maxime illo perferendis? Facere sit ut cum ipsa dignissimos fugiat officiis doloribus omnis deserunt facilis recusandae dicta perferendis sapiente rem similique praesentium rerum, nisi, voluptas quae. Nisi nobis dolorem aliquam quos deserunt quod tempora alias eligendi quaerat tempore ipsa vero impedit voluptates fuga officia sequi assumenda iusto est, perferendis exercitationem obcaecati expedita neque unde beatae! Numquam, exercitationem. Facere, soluta quidem! Odit unde architecto eveniet incidunt illo maiores recusandae beatae est dolor sapiente dicta provident laboriosam nulla earum ad fugiat deleniti a nihil iusto perspiciatis temporibus corporis vitae, minus magnam! Laudantium ut quidem eaque quisquam debitis. Sint, architecto placeat repellat ipsum eum dolore dolorum necessitatibus! Numquam tempore, tempora eaque modi illum aliquid nemo reiciendis est eveniet adipisci provident enim quidem autem dolorum corrupti ex, culpa rerum veritatis! Nesciunt, cupiditate deserunt. Aliquid in, ipsa, ab doloribus, nostrum vero sapiente animi nihil mollitia ratione nulla pariatur quo ut inventore exercitationem porro explicabo illum tenetur molestias laudantium!</p>
+  <article class="pt-5 px-3">
+  <p>Empresa dedicada a la captación, depuración venta y distribución de agua en bidones.
+  </p>
+<p>La planta ubicada en la Av. Italia 3374 cuenta con equipo semi automático LTC-120 de Santini S.A., de llenado de bidones, además de un equipo ozonizador con torre de mezcla, marca Ozona. </p>
+<p><strong>La actividad que desarrolla es la venta de bidones de agua.</strong></p>
+</article>
 </section>
 <?php
 include("includes/footer.php");
