9ad58ae9fd403099aabf369ed3bbcaa80cf10a0d
Leonardo Alvarez <leo92alvarez25@gmail.com>
2024-06-05 22:41:00 -0300

    correccion tipado

--- js/registrar.js
+++ js/registrar.js
@@ -126,7 +126,7 @@ $(document).ready(function () {
           $("#emailEmployee").val("");
 
         }
-        if(response=== "No se pudo crear el empleado."){
+        if(response === "No se pudo crear el empleado."){
           $("#mensaje").html(`<div class="alert alert-dismissible alert-danger">
             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
             <strong>Error al Crear un Registro.</strong>
