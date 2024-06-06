e24e4fc5fe457fbdb337a45c6e61f3d3542beec1
Leonardo Alvarez <leo92alvarez25@gmail.com>
2024-06-05 22:38:21 -0300

    cambio consulta y sesion

--- gestionLogin.php
+++ gestionLogin.php
@@ -7,13 +7,14 @@ if (!empty($_POST['email']) && !empty($_POST['contrasena'])) {
   $email = $_POST['email'];
   $contrasena = $_POST['contrasena'];
 
-  $sql = "SELECT nombre, apellido, password, nombre_usuario, id_rol FROM `empleados` WHERE `correo` = '$email' OR `nombre_usuario` = '$email'";
+  $sql = "SELECT nombre, apellido,password, correo, nombre_usuario, id_rol FROM `empleados` WHERE `correo` = '$email' OR `nombre_usuario` = '$email'";
   $result = mysqli_query($connection, $sql);
   $user = mysqli_fetch_array($result);
   if (mysqli_num_rows($result) == 1  && password_verify($contrasena, $user['password'])) {
     $_SESSION['nombre_usuario'] = $user['nombre_usuario'];
     $_SESSION['nombreCompleto'] = $user['nombre'] . " " . $user['apellido'];
     $_SESSION['id_rol'] = $user['id_rol'];
+    $_SESSION['correo'] = $user['correo'];
     obtenerConsultasSinLeer();
     echo "success";
   } else {
