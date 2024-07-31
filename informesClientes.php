fa3f817802841dab2028312645fa7cbd96a561ab
Héctor Mendoza <hectormendo@gmail.com>
2024-07-31 15:32:56 -0300

    cambios hector

--- informesClientes.php
+++ informesClientes.php
@@ -8,22 +8,17 @@ if (!isset($_SESSION['id_rol'])) {
 
 $nombre_usuario = $_SESSION['nombre_usuario'];
 ?>
-<h2> Hola <?= $_SESSION["nombreCompleto"] ?></h2>
+
+<h2> Explotaciones <?= $_SESSION["nombreCompleto"] ?></h2>
 <div id="informes">
-  <h3 class="text-center">informes</h3>
+  <h3 class="text-center">I N F O R M E S</h3>
   <ul>
-    <li><a href="3">Lorem ipsum dolor sit amet consectetur.</a></li>
-    <li><a href="3">Maxime possimus saepe amet nobis dolorem!</a></li>
-    <li><a href="">Nesciunt pariatur recusandae nulla vero commodi.</a></li>
-    <li><a href="">Assumenda temporibus adipisci inventore nisi expedita.</a></li>
-    <li><a href="">Rem laudantium ex voluptatum dolores fuga.</a></li>
-    <li><a href="">Ab id nulla quaerat quo iusto!</a></li>
-    <li><a href="">Eaque quae nesciunt beatae dolore harum?</a></li>
-    <li><a href="">Amet dolor eaque vero quod laboriosam!</a></li>
-    <li><a href="">Nihil magni dignissimos corrupti iure nisi!</a></li>
-    <li><a href="">Eveniet perferendis ipsum corrupti impedit ullam.</a></li>
+    <li><a href="listado_pedidos.php">* LISTADO DE PEDIDOS CON FACTURACION ENTRE FECHAS *</a></li>
+    <li><a href="3">* LISTADO DE PEDIDOS ENTRE FECHAS *</a></li>
+    <li><a href="">* LISTADO DE PEDIDOS NO ENTREGADOS ENTRE FECHAS *</a></li>
   </ul>
 </div>
+
 <?php
 include("includes/footer.php");
 ?>
